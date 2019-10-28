34a5cac3177308aebb3e500dc7d2e3765e1c3cce
Piyush Jain <piyush.jain5@cognizant.com>
2019-10-28 23:43:46 +0530

    Added react app

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// Components
+import BookList from './components/BookList';
+
+function App() {
+  return (
+    <div id="main">
+      <h1>Piyush Jain</h1>
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
